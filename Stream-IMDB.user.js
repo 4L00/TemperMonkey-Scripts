fd990ded1d513f45ce94c6ece3a2d0f86a1c6c15
4L00 <57715285+4L00@users.noreply.github.com>
2019-12-09 20:16:27 +0530

    Updated the Script, added yotube button to watch trailers in new tab

--- Stream-IMDB.user.js
+++ Stream-IMDB.user.js
@@ -15,6 +15,8 @@
     var loc = window.location.href;
     var lst = loc.split("/");
     var tt = lst[4];
+    var sa = document.getElementsByTagName("title")[0].innerText;
+    console.log(sa);
 
     var btn1 = document.createElement("BUTTON");
     btn1.id ='vidsrcme';
@@ -32,11 +34,22 @@
     btn2.classList.add('large');
     btn2.classList.add('primary');
 
+    var btn3 = document.createElement("BUTTON");
+    btn3.id ='yotube';
+    btn3.innerHTML = "▶️ Trailer on YouTube";
+    btn3.style.margin = '0px 20px 20px';
+    btn3.classList.add('bt');
+    btn3.classList.add('secondary');
+
     var x = document.getElementsByClassName('pro_title_link_with_separator')[0];
     insertBefore(btn1, x);
 
     var y = document.getElementsByClassName('pro_title_link_with_separator')[0];
     insertBefore(btn2, y);
+
+    var z = document.getElementsByClassName('pro_title_link_with_separator')[0];
+    insertBefore(btn3, z);
+
 
     document.getElementById("vidsrcme").addEventListener("click", function(){
   window.open("https://vidsrc.me/embed/" + tt + "/");
@@ -44,5 +57,9 @@
 
    document.getElementById("gdriveplayer").addEventListener("click", function(){
   window.open("https://database.gdriveplayer.us/player.php?imdb=" + tt);
+});
+
+   document.getElementById("yotube").addEventListener("click", function(){
+  window.open("https://www.youtube.com/results?search_query=" + sa);
 });
 })();
